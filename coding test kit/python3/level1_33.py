8bc9676cd23662ae291a6acebc4b08640883a905
leewatermin <suminlee228@gmail.com>
2022-02-26 22:25:30 +0900

    level1 최대공약수와 최소공배수 add

---
+++ coding test kit/python3/level1_33.py
@@ -0,0 +1,6 @@
+def solution(n, m):
+    for i in range(min(n, m), 0, -1):
+        if n % i == 0 and m % i == 0: 
+            for j in range(max(n, m), n*m+1):
+                if j % n == 0 and j % m == 0: return [i, j]
+           
