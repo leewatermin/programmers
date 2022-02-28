8c4ef08909534c055f49e392a07ddca6ed086240
leewatermin <suminlee228@gmail.com>
2022-02-28 22:54:53 +0900

    level1 신고 결과 받기 add

---
+++ coding test kit/python3/level1_34.py
@@ -0,0 +1,5 @@
+def solution(id_list, report, k):
+    s1 = [s.split()[1] for s in set(report)]
+    s2 = [s for s in set(s1) if s1.count(s) >= k]
+    s3 = [s.split()[0] for s in set(report) if s.split()[1] in s2]
+    return [s3.count(i) for i in id_list]
